a9e7f8bd82051ad514fb2ec6b7ba77d15186104e
David Gowers <finticemo@gmail.com>
2014-12-15 17:05:01 +1030

    repair: new: changed_paths(), with CLI. (dry-run only) update_file_metadata CLI also added.
    changed_paths() detects various error/warning conditions in the `file` table,
    categorizes and records them in a {category:[filenames]} dictionary.
    The CLI, without --json, simply reports stats on these.
    With --json, it also outputs a JSON file to stdout.
    
    Categories of error/warning condition include:
    
    * badmtime (mtimes off by <1s. Should be fixed by updating mtimes
                to match on-disk mtimes for the paths)
    * broken (broken symlink. Usually needs manual fixing or removal)
    * modified (mtimes differ by >1s, path is probably modified and needs re-fingerprinting)
    * deleted (path no longer exists.
               taggings should be merged with an existing duplicate if possible,
               then the path should be removed from file_tag and file tables)
    * type (path pointed to a directory, but now points to a file, or vice versa)

--- tmsoup/repair.py
+++ tmsoup/repair.py
@@ -2,8 +2,12 @@ import os
 import glob
 import re
 import sys
-from .core import splitpath, KeyExists, file_mtime, file_ids, file_tags, resolve_tag_value
-from .fingerprint import set_fingerprint_algorithm, get_fingerprint_algorithm, fingerprint
+from .core import (splitpath, KeyExists,
+                   file_mtime, file_ids, file_tags,
+                   file_info, resolve_tag_value)
+from .fingerprint import (set_fingerprint_algorithm,
+                          get_fingerprint_algorithm,
+                          fingerprint)
 
 INVALID_SIZE = -1
 
@@ -201,67 +205,129 @@ def change_fingerprint_algorithm(cursor, algorithm):
     cursor.connection.commit()
 
 
-def parse_args(args):
-    from tmsoup.core import _add_database_option, get_db_path
-    from argparse import ArgumentParser
-    parser = ArgumentParser(description='Repair or check duplicates in TMSU database')
-    _add_database_option(parser)
-    subp = parser.add_subparsers(dest='cmd')
-    subp.required = True
-    resolve = subp.add_parser('dupes',
-                              help='Find and resolve duplicate files.'
-                              ' During resolution, the taggings on each file'
-                              ' in a set of duplicates is tracked. '
-                              ' When the set is reduced to 1 member, '
-                              ' via deletion, taggings from the other'
-                              ' members are merged into the taggings of the'
-                              ' remaining member.')
-    group = resolve.add_mutually_exclusive_group()
-    group.add_argument('-H', '--keep-highest',
-                         help='Rather than interactively deciding what to'
-                         ' delete, select the member with the highest score'
-                         ' (categorizability * descriptiveness of filename)'
-                         ' and remove all the other items in each set')
-    group.add_argument('-c', '--command',
-                         help='Invoke this command to interactively'
-                         ' remove duplicates. It should include a function'
-                         ' to delete files from disk.',
-                         default=None)
-    group.add_argument('-s', '--stats',
-                         help='Show stats about duplicates')
-
-    resolve.add_argument('-1', '--single',
-                         default=False, action='store_true',
-                         help='Invoke the command once per set of dupes,'
-                         ' rather than accumulating several sets in a batch')
-    resolve.add_argument('-i', '--stdin',
-                         default=False, action='store_true',
-                         help='Pass file list to command via stdin, '
-                         'rather than via commandline arguments.')
-    resolve.add_argument('-m', '--minimum',
-                         default=2, type=int,
-                         help='Minimum items that actually exist in'
-                         ' a set of files, before that set will be considered'
-                         ' for interactive processing. Default %(default)d.'
-                         ' Use 0 if you want all sets, even ones that consist'
-                         ' solely of deleted files.'
-                         )
-
-    parser.set_defaults(limit=300)
-    args = parser.parse_args(args)
-    args.database = get_db_path(args.database)
-    if not args.command and (args.single or args.stdin):
-        print ('--single and --stdin require --command')
-        import sys
-        sys.exit(1)
-    if args.stdin:
-        args.limit = 0xffff
-    return args
-
-
 def _msg(*args,**kwargs):
     import sys
     print(*args, file=sys.stderr, **kwargs)
+
+
+def changed_paths(cursor):
+    """Return a dict categorizing different types of dubious path.
+
+    Paths checked include only those that are recorded in the database.
+
+    Modified paths: have a newer mtime than the database record
+    Broken paths: point at broken symlinks
+    Deleted paths: point at paths that don't exist.
+    Kind paths: changed from a directory to a file or vice versa
+    Badmtime paths: recorded mtime is in the 'old' format,
+                    and comparing mtimes in 'old' format reveals no
+                    actual mtime change.
+                    it is appropriate to update all such paths
+                    to use the new date format -> nanosecond accuracy.
+
+
+    Notes
+    ======
+    The 'known good' mtime format is similar to stat, except in GMT time
+    rather than local time:
+
+      2012-09-03 13:35:18.791237225
+
+    where the decimal part may be truncated or omitted according to the
+    number of trailing zeroes.
+
+    This function does not check for paths that
+    have the same mtime but different fingerprints.
+
+    """
+    import sys
+    i = 0
+    modified = []
+    broken = []
+    deleted = []
+    kind = []
+    badmtime = []
+
+    for dirname, filename, omtime, isdir in \
+        cursor.execute('SELECT directory, name, mod_time,'
+                       ' is_dir'
+                       ' FROM file'):
+        i += 1
+        if (i % 100) == 0:
+            sys.stderr.write('%08d\r' % i)
+        p = os.path.join(dirname, filename)
+
+        if os.path.islink(p) and not os.path.exists(p):
+            broken.append(p)
+            continue
+        elif not os.path.exists(p):
+            deleted.append(p)
+            continue
+
+        mtime = file_mtime(p)
+
+        opos = omtime.find('.')
+        pos = mtime.find('.')
+        trunc_eq = omtime[:opos] == mtime[:pos]
+
+        # time differs by <1s. Probably an incorrect record.
+        if trunc_eq and not (omtime == mtime):
+            badmtime.append(p)
+            omtime=mtime
+
+        if omtime != mtime:
+            modified.append(p)
+        isdir = (isdir != 0)
+        if isdir != os.path.isdir(p):
+            kind.append(p)
+
+    return {'modified' : modified,
+            'broken': broken,
+            'deleted': deleted,
+            'type': kind,
+            'badmtime': badmtime}
+
+
+def update_file_metadata(cursor, paths):
+    """Explicitly recalculate fingerprint and mtime for each path,
+    updating the database with the new values.
+
+    Raises
+    =======
+    OSError     when the current path is a directory and the recorded one
+                was a file, or vice versa.
+    IOError     when the number of results for a given path is not exactly 1.
+    """
+    old_info = {}
+    for p in paths:
+        finf = file_info(p)
+        tmp = cursor.execute('SELECT is_dir, fingerprint, mod_time'
+                             ' FROM file'
+                             ' WHERE directory = ? AND name = ?',
+                             finf).fetchone()
+        if not tmp:
+            raise IOError('Expected 1 row for {},'
+                          ' but got {}'.format(p, cursor.rowcount))
+
+        is_dir, fp, mtime = tmp
+        old_info[p] = (fp, mtime)
+        is_dir = (is_dir != 0)
+        now_is_dir = os.path.isdir(p)
+        if is_dir != now_is_dir:
+            raise OSError('Directory-ness changed. was {}, now {}'.format(
+                              is_dir, now_is_dir))
+    # okay, nothing looks hinky, let's recalculate
+    algo = get_fingerprint_algorithm(cursor)
+    updated = {}
+    for p in paths:
+        fp = fingerprint(p, algo)
+        mtime = file_mtime(p)
+        updated[p] = (fp, mtime)
+    # we don't actually update the database yet, just show our work.
+    print('Recalculated values:')
+    print('\n'.join('%s %s %s' % (v[0], v[1], k) for k,v in updated.items()))
+    print('Records differed for:')
+    print('\n'.join(k for k, v in updated.items() if v != old_info[k]))
 
 def _interactive_duplicate_removal(cursor, command, limit, minimum=2):
     from itertools import chain
@@ -365,6 +431,88 @@ def _interactive_duplicate_removal(cursor, command, limit, minimum=2):
     sys.exit(0)
     apply_queued_taggings()
 
+def parse_args(args):
+    from tmsoup.core import _add_database_option, get_db_path
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description='Repair or check duplicates in TMSU database')
+    _add_database_option(parser)
+    subp = parser.add_subparsers(dest='cmd')
+    subp.required = True
+    check = subp.add_parser('check',
+                             help='Show stats about'
+                                  ' broken/modified/deleted/type-changed'
+                                  ' paths')
+    check.add_argument('-j', '--json',
+                         default=False, action='store_true',
+                         help='Output full data'
+                              ' (categorized lists of filenames)'
+                              ' in JSON format')
+    update = subp.add_parser('update',
+                             help='Explicitly update the hash and mtime of'
+                             ' specified file(s).'
+                             ' Useful for known cosmetic alterations --'
+                             ' for example, rotating or flipping an image')
+    update.add_argument('files', help='Files to update hash+mtime for',
+                        nargs="+")
+    resolve = subp.add_parser('dupes',
+                              help='Find and resolve duplicate files.'
+                              ' During resolution, the taggings on each file'
+                              ' in a set of duplicates is tracked. '
+                              ' When the set is reduced to 1 member, '
+                              ' via deletion, taggings from the other'
+                              ' members are merged into the taggings of the'
+                              ' remaining member.')
+    group = resolve.add_mutually_exclusive_group()
+    group.add_argument('-H', '--keep-highest',
+                         help='Rather than interactively deciding what to'
+                         ' delete, select the member with the highest score'
+                         ' (categorizability * descriptiveness of filename)'
+                         ' and remove all the other items in each set')
+    group.add_argument('-c', '--command',
+                         help='Invoke this command to interactively'
+                         ' remove duplicates. It should include a function'
+                         ' to delete files from disk.',
+                         default=None)
+    group.add_argument('-s', '--stats',
+                         help='Show stats about duplicates')
+
+    resolve.add_argument('-1', '--single',
+                         default=False, action='store_true',
+                         help='Invoke the command once per set of dupes,'
+                         ' rather than accumulating several sets in a batch')
+    resolve.add_argument('-i', '--stdin',
+                         default=False, action='store_true',
+                         help='Pass file list to command via stdin, '
+                         'rather than via commandline arguments.')
+    resolve.add_argument('-m', '--minimum',
+                         default=2, type=int,
+                         help='Minimum items that actually exist in'
+                         ' a set of files, before that set will be considered'
+                         ' for interactive processing. Default %(default)d.'
+                         ' Use 0 if you want all sets, even ones that consist'
+                         ' solely of deleted files.'
+                         )
+
+    parser.set_defaults(limit=300)
+    args = parser.parse_args(args)
+    args.database = get_db_path(args.database)
+    if args.cmd == 'dupes':
+        if not args.command and (args.single or args.stdin):
+            print ('--single and --stdin require --command')
+            import sys
+            sys.exit(1)
+        if args.stdin:
+            args.limit = 0xffff
+    elif args.cmd == 'update':
+        missing_files = [f for f in args.files if not os.path.exists(f)]
+        if missing_files:
+            _msg('Update: the following files were not found on disk: %s'
+                  % (" ".join(sorted(missing_files))))
+            _msg('Aborting update.')
+            sys.exit(1)
+    return args
+
+
 def main(argv):
     from plumbum.cmd import which
     import sqlite3
@@ -389,9 +537,22 @@ def main(argv):
                 args.command,
                 args.limit,
                 args.minimum)
-
-
+    elif args.cmd == 'update':
+        conn = sqlite3.connect(args.database)
+        update_file_metadata(conn.cursor(), args.files)
+    elif args.cmd == 'check':
+        conn = sqlite3.connect(args.database)
+        data = changed_paths(conn.cursor())
+        summarystream = sys.stdout
+        if args.json:
+            import json
+            json.dump(data, sys.stdout)
+            summarystream = sys.stderr
+        for k, v in sorted(data.items(), key=lambda i:i[0]):
+            print('%-10s : %6d' % (k.title(),len(v)), file=summarystream)
 
 if __name__ == '__main__':
     import sys
     main(sys.argv[1:])
+
+__all__ = ('update_file_metadata',)
